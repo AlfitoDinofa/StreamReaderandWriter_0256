afffd5899ca06ee3f5cd779fbf8885a9d0753275
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-06-10 11:08:35 +0700

    membuat integer utama dan menambahkan fungsi string

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -3,3 +3,20 @@
 #include <string>
 using namespace std;
 
+int main(){
+    string baris;
+    string NamaFile;
+
+    cout << "masukkan nama file : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+    
+
+}
