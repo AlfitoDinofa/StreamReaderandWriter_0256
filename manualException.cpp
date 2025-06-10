c4af1745c3d908c042a40a05c383124639b5cdde
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-06-10 11:20:32 +0700

    block yang akan dieksekusi

--- manualException.cpp
+++ manualException.cpp
@@ -6,5 +6,16 @@ int main(){
         cout << "selamat belajar di prodi TI UMY" << endl;
         throw 5; //melemparkan sebuah integer maka,
         cout << "pernyataan tidak akan diesekusi" << endl;
-}
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "pengecualian akan dieksekusi" << endl;
+
+    }
+    catch(...){
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "default pengecualian dieksekusi" << endl;
+        
+    }
+
 }
