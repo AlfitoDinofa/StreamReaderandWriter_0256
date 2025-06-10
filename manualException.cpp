b401ed80b4a0d62ec31b0ad55d364519117ee24b
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-06-10 11:18:01 +0700

    menampilkan pernyataan tidak akan deisekusi

--- manualException.cpp
+++ manualException.cpp
@@ -5,4 +5,6 @@ int main(){
     try {
         cout << "selamat belajar di prodi TI UMY" << endl;
         throw 5; //melemparkan sebuah integer maka,
+        cout << "pernyataan tidak akan diesekusi" << endl;
+}
 }
