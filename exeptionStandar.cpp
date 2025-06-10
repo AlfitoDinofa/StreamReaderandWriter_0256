d1d0c824b8c306b617128ae062f4798d917e53db
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-06-10 11:21:40 +0700

    menambahkan array

--- exeptionStandar.cpp
+++ exeptionStandar.cpp
@@ -7,5 +7,13 @@ using namespace std;
 
 int main()
 {
+    cout << "awal program" << endl; //penanda 1:....
+    try {
+        array<int, 3> data = { 10, 20, 30 };
+        //int data[3] = {10, 20, 30,}
+        //pesan array integer 3 elemen
+        cout <<data.at(5)<<endl;
+        //memnaggil array elemen ke 5
+    }
   
 }
