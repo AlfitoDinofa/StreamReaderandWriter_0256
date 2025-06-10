5a5ccab6db45f2b5c30c9e291127cd004e8a507e
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-06-10 11:22:05 +0700

    menampilkan baris program terakhir

--- exeptionStandar.cpp
+++ exeptionStandar.cpp
@@ -15,5 +15,14 @@ int main()
         cout <<data.at(5)<<endl;
         //memnaggil array elemen ke 5
     }
-  
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        //akan dieksekusi karna array data hanya memliki 3 elemen
+        
+    }
+    cout << "baris program yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa henti meskipun
+    terjadi kesalahan*/
+    return 0;
 }
